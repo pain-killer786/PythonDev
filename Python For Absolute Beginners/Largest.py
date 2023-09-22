1975be29bb961800436f2bfa22b3ce4c335bef31
merikh786 <krishnendunandy43@gmail.com>
2023-09-22 23:25:53 +0530

    Done

---
+++ Python For Absolute Beginners/Largest.py
@@ -0,0 +1,10 @@
+a=(int(input("Enter a number")))
+b=(int(input("Enter a number")))
+c=(int(input("Enter a number")))
+if (a>b>c):
+    print("The largest number is",a)
+elif(b>a>c):
+    print("The largest number is",b)
+else:
+    print("The largest number is",c)
+    
