f6125691da3732efe7691a2b21714f4f8aa4d486
merikh786 <krishnendunandy43@gmail.com>
2023-09-25 18:30:03 +0530

    Done

---
+++ Python For Absolute Beginners/Dictionaries/Declaration.py
@@ -0,0 +1,23 @@
+#dic={"name":20}
+
+# Dictionaries are ordered
+# Dictionaries are Changeable
+# Dictionaries are unindexed
+# Dictionaries doesn't allow duplicates
+# Dictionaries can store any datatype
+
+phones={"john": 982528,
+        "Rai":46285,
+        "Joy": 12345
+        }
+#printing a dictionary
+print(phones)
+print(len(phones))#Check length of Dictionary
+print(type(phones)) #Check type of Dictionary
+print(phones["john"])
+print(phones.get("john"))
+print(phones.keys())
+
+#Update value in Dict
+phones["john"]= 9874
+print(phones)
