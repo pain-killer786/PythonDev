976806b5e377b8ca7583c885800b6a14c015af83
merikh786 <krishnendunandy43@gmail.com>
2023-09-25 20:22:25 +0530

    DOne

---
+++ Python For Absolute Beginners/Functions/Declaration.py
@@ -0,0 +1,7 @@
+# Functions are blocks of reusable code that performs a specific task
+# input --> function --> output
+
+#Types of Function:- 1. Built-in   2.User defined
+# <Syntax> -> def function_name(parameters):
+#      #statement
+#      return expression
