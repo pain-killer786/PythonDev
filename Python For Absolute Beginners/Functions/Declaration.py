3bc19f6c4e108d284cb64049e67f01cb41eb0b34
merikh786 <krishnendunandy43@gmail.com>
2023-09-30 21:48:00 +0530

    Done

--- Python For Absolute Beginners/Functions/Declaration.py
+++ Python For Absolute Beginners/Functions/Declaration.py
@@ -5,3 +5,27 @@
 # <Syntax> -> def function_name(parameters):
 #      #statement
 #      return expression
+
+#positional argument
+#print("The sum is",Sum(3,2))
+
+#keyword argument(named arguments)
+#print("The sum is: ",Sum(n2=6,n1=4))
+
+#Arbitary arguments -> When we don't know how many arguments we have to pass, we use *args
+
+def addAllNumbers(*args):
+    sum=0
+    for i in args:
+        sum+=i
+    return sum
+
+output=addAllNumbers(2,4,5,6,9,8,7,4,5,2,1,3,6,5,4,7)
+print(output)
+
+# **kwargs-> key value pair arguments like used in dictionary
+def studentInfo(**kwargs):
+    for x,y in kwargs.items():
+        print(x,y)
+        
+studentInfo(name="Krishnendu",age=21,city="Kol")
--- Python For Absolute Beginners/Functions/SumofNumbers.py
+++ Python For Absolute Beginners/Functions/SumofNumbers.py
@@ -6,26 +6,3 @@ a=int(input("Enter a number"))
 b=int(input("Enter a number"))
 print("The Sum is:",Sum(a,b))
 
-#positional argument
-#print("The sum is",Sum(3,2))
-
-#keyword argument(named arguments)
-#print("The sum is: ",Sum(n2=6,n1=4))
-
-#Arbitary arguments -> When we don't know how many arguments we have to pass, we use *args
-
-def addAllNumbers(*args):
-    sum=0
-    for i in args:
-        sum+=i
-    return sum
-
-output=addAllNumbers(2,4,5,6,9,8,7,4,5,2,1,3,6,5,4,7)
-print(output)
-
-# **kwargs-> key value pair arguments like used in dictionary
-def studentInfo(**kwargs):
-    for x,y in kwargs.items():
-        print(x,y)
-        
-studentInfo(name="Krishnendu",age=21,city="Kol")
