b1e30c88aa57a51ea136d0ceda01125e3119ee76
merikh786 <krishnendunandy43@gmail.com>
2023-09-30 21:41:01 +0530

    Done

--- Python For Absolute Beginners/Functions/SumofNumbers.py
+++ Python For Absolute Beginners/Functions/SumofNumbers.py
@@ -11,3 +11,14 @@ print("The Sum is:",Sum(a,b))
 
 #keyword argument(named arguments)
 #print("The sum is: ",Sum(n2=6,n1=4))
+
+#Arbitary arguments -> When we don't know how many arguments we have to pass, we use *args
+
+def addAllNumbers(*args):
+    sum=0
+    for i in args:
+        sum+=i
+    return sum
+
+output=addAllNumbers(2,4,5,6,9,8,7,4,5,2,1,3,6,5,4,7)
+print(output)
