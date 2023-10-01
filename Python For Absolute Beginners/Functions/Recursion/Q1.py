477de1f0608b54dc2574a5023fc9f271ad3f91e0
merikh786 <krishnendunandy43@gmail.com>
2023-10-01 22:46:47 +0530

    Done

---
+++ Python For Absolute Beginners/Functions/Recursion/Q1.py
@@ -0,0 +1,11 @@
+# Write a program to Print numbers from n to 1.
+   
+def printNumbers(x):
+    if x==0:
+        return
+    
+    print (x)
+    printNumbers(x-1)
+    
+n=int(input("Enter a number"))
+print("The result is", printNumbers(n))
