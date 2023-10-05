be0368aa2070ca5e510589fe14e1587a6663e0af
merikh786 <krishnendunandy43@gmail.com>
2023-10-05 17:41:31 +0530

    Done

--- Python For Absolute Beginners/OOPS/Introduction.py
+++ Python For Absolute Beginners/OOPS/Introduction.py
@@ -1,2 +1,7 @@
 # What is Object Oriented Programming
 # -> Object oriented programming is a programming paradigm where the software design revolves around objects/data rather than functions.
+
+#Why use OOPs in Python?
+# Helps to mimic real world entities & their interactions
+# Code reusability
+# Organisation & maintainability of code
