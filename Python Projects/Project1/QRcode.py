cbbeced84ffc42faba316acba69a231235949aac
White-Knight786 <krishnendunandy43@gmail.com>
2025-02-13 19:40:24 +0530

    Delete Python Projects directory

--- Python Projects/Project1/QRcode.py
+++
@@ -1,5 +0,0 @@
-# Make a QR code in python 
-
-import qrcode as qr
-img= qr.make("https://www.facebook.com/the.whiteknight786")
-img.save("Facebook_QR.png")
--- Python Projects/Project1/Qrcode1.py
+++
@@ -1,5 +0,0 @@
-# Make a QR code in python 
-
-import qrcode as qr
-img= qr.make("https://sampurnaimagine.com")
-img.save("Sampurna_QR.png")
